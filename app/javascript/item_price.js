ce97cd70e7e7bb0284a9143b9f1e61a86073f161
santakukoumei <rx0gtrsv@gmail.com>
2022-01-06 19:43:18 +0900

    販売手数料・利益計算の実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,11 @@
+window.addEventListener('input', () => {
+  const priceInput = document.getElementById("item-price");
+  const inputValue = priceInput.value;
+  console.log(inputValue);
+
+  const addTaxDom = document.getElementById("add-tax-price");
+  addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
+
+  const addTaxProfit = document.getElementById("profit");
+  addTaxProfit.innerHTML = Math.floor (inputValue * 0.9)
+});
