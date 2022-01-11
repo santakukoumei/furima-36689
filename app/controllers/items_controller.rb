2eb690bae071b48ed5afa8dcadf07c52eaeb3884
santakukoumei <rx0gtrsv@gmail.com>
2022-01-12 01:34:26 +0900

    URLからの商品情報編集ページの遷移先設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show]
-
+  before_action :set_item, only: [:edit, :show]
+  before_action :contributor_confirmation, only: [:edit, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -27,13 +27,13 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
+    
   end
 
   def update
-    @item = Item.find(params[:id])
+    
     if @item.update(item_params)
-      redirect_to item_path(@item)
+      redirect_to item_path
     else
       render :edit
     end
@@ -51,6 +51,10 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless @item.user == current_user
   end
 
 end
