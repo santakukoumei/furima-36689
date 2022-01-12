dc01923479ba2e52bce7b91d48f9870941af863a
santakukoumei <rx0gtrsv@gmail.com>
2022-01-12 19:20:20 +0900

    before_action :set_item内の記述修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show]
+  before_action :set_item, only: [:edit, :show, :update]
   before_action :contributor_confirmation, only: [:edit, :update]
 
   def index
